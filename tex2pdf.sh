070761faa5b5a782bc23905ea5e4718b3f8a1e81
alapha23 <alapha23@gmail.com>
2018-02-14 03:28:52 +0900

    helloworld

---
+++ tex2pdf.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+myfile=$1
+
+if [ -z $1 ]; then
+echo "Usage: bash tex2pdf.sh <filename> "
+	exit 1;
+fi
+
+pdflatex $myfile
+
+rm *.aux
+#latex $myfile
+#dvips -o ${myfile}.ps $myfile
+#ps2pdf -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dEmbedAllFonts=true -dSubsetFonts=true -dMaxSubsetPct=100 ${myfile}.ps ${myfile}.pdf
